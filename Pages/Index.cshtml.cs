4597a32b45f078fbdb4c37d93573d9d8c05c9627
jim bergstrom <jim@bergles.xyz>
2021-05-07 07:52:12 -0600

    Initial demo commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Iolaus.Routes;
+using Iolaus;
+using System.Text.Json;
+using System.Reactive.Linq;
+
+namespace IolausDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly Router _router;
+
+        [BindProperty]
+        public int NumDice { get; set; }
+        [BindProperty]
+        public int SidesPerDie { get; set; }
+        [BindProperty]
+        public bool Stats { get; set; }
+
+        public string Reply { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, Router router)
+        {
+            _logger = logger;
+            _router = router;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPost()
+        {
+            var message = new {type = "dice", cmd = "roll", NumDice, SidesPerDie, Stats};
+            var serialized = JsonSerializer.Serialize(message);
+            var m = Message.Parse(serialized).Unsafe();
+
+            var send = _router.GetFunc(m);
+            var stream = send(m);
+
+            var firstResponse = await stream.FirstAsync();
+            Reply = firstResponse.Match(
+                Some: (msg) => msg.ToString(),
+                None: () => "No Reply"
+            );
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+using Iolaus.Routes;
+using Iolaus.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NATS.Client;
+
+namespace IolausDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddHttpClient();
+
+            var connectionFactory = new ConnectionFactory();
+            var connection = connectionFactory.CreateConnection();
+            services.AddSingleton(connection);
+
+            var configReader = new Iolaus.Config.FileConfigurationProvider("config.json");
+            services.AddSingleton<Iolaus.Config.IConfigurationProvider>(configReader);
+
+            services.AddRouteRegistry(r => {
+                r.Add("NATS", Iolaus.Routes.Nats.NatsRoute.Route);
+                r.Add("HTTP", Iolaus.Routes.Http.HttpRoute.Route);
+            });
+
+            services.AddSingleton<Router>();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
